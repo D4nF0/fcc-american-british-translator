eebbf7f2a85ada2b934131c57fb73b5b8751adcc
Daniel Fořt <danielfort2005@gmail.com>
2024-11-06 18:18:25 +0000

    feat: add api logic

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,32 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+      let translatedText = "";
+
+      if( !locale || text == undefined ){
+        res.send({ error: 'Required field(s) missing' });
+        return;
+      }
+
+      if( text == "" ){
+        res.send({ error: 'No text to translate' });
+        return;
+      }
+
+      if( locale == "american-to-british" ){
+        translatedText = translator.toBritish( text );
+      } else if( locale == "british-to-american" ){
+        translatedText = translator.toAmerican( text );
+      } else {
+        res.send({ error: 'Invalid value for locale field' });
+        return;
+      }
+
+      if( !translatedText || translatedText == text ){
+        res.send({ text, translation: "Everything looks good to me!" })
+      } else {
+        res.send({ text, translation: translatedText });
+      }
     });
 };
